8f5956a2902c2010b13ea996d8b49afe8c08d651
Nick Jantz <thenickjantz@gmail.com>
2025-03-13 20:40:17 -0500

    fix webpack config to work with __dirname

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -1,10 +1,14 @@
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   entry: "./src/index.tsx",
   output: {
-    path: path.resolve(path.resolve(), "dist"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     clean: true
   },
