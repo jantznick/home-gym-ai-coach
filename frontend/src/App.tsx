11854631f58608faa4e79e47613075f10523a8e2
Nick Jantz <thenickjantz@gmail.com>
2025-03-14 21:08:20 -0500

    Revert "some ai frontend work"
    This reverts commit 6c3664247c2a8791fd254888b910dcd2ec8309d6.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -6,31 +6,18 @@ import About from "./pages/About";
 import Login from "./pages/Login";
 import OAuthCallback from "./pages/OAuthCallback";
 import Dashboard from "./pages/Dashboard";
-import TermsOfService from "./pages/TermsOfService";
-import Header from "./components/Header";
-import LoginRegisterInterstitial from "./components/LoginRegisterInterstitial";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import Toast from "./components/Toast";
 
 const App: React.FC = () => {
-  const [toastMessage, setToastMessage] = useState("");
-
   return (
-    <>
-      <Toast message={toastMessage} />
     <Router>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/login" element={<LoginRegisterInterstitial />} />
+		<Route path="/login" element={<Login />} />
         <Route path="/callback" element={<OAuthCallback />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/terms-of-service" element={<TermsOfService />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
     </Router>
-    </>
   );
 };
 
